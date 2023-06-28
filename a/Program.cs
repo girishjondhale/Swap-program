26832100daa18ac6c4f5b51f191d7c3e4d258fc5
girishjondhale <girishjondhale1997@gmail.com>
2023-06-28 05:35:25 -0700

    Add project files.

---
+++ a/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 10,b= 20,temp;
+temp = a;
+a = b;
+b= temp;
+
+Console.WriteLine("values after swap");
+Console.WriteLine("a=" + a);
+Console.WriteLine("b=" + b);
